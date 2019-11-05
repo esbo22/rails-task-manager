0c16268153dea0614751e91c66ef2dcc8a2413d0
Johnny Girault <esbo@hotmail.fr>
2019-11-05 16:28:59 +0100

    terminer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index', as: 'index'
+  root to: 'pages#home'
+  # READ
+  get 'tasks', to: 'tasks#index'
+
+  # CREATE
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to:'tasks#create'
+
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update', as: 'task'
+
+  # READ BIS
+  get 'tasks/:id', to: 'tasks#show'
+
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
